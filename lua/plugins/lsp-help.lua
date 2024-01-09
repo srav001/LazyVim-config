9a764574de2293702d3e65b4f8cf4247af6730ea
Sravan <sravansuresh1x@gmail.com>
2024-01-09 23:47:20 +0800

    feat: add inlay hints

--- lua/plugins/lsp-help.lua
+++ lua/plugins/lsp-help.lua
@@ -10,4 +10,8 @@ return {
 		-- opts will be merged with the parent spec
 		opts = { use_diagnostic_signs = true },
 	},
+	{
+		"lvimuser/lsp-inlayhints.nvim",
+		opts = {},
+	},
 }
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,5 +1,22 @@
 return {
-	{ import = "lazyvim.plugins.extras.coding.copilot" },
+	{
+		"zbirenbaum/copilot.lua",
+		event = { "InsertEnter" },
+		cmd = { "Copilot" },
+		opts = {
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					accept = "<C-l>",
+					accept_word = false,
+					accept_line = false,
+					dismiss = "<C-]>",
+				},
+			},
+		},
+	},
 	{
 		"echasnovski/mini.move",
 		version = "*",
