1a389898d3c6c11d82dcfa74273203acd2c95591
Sravan <sravansuresh1x@gmail.com>
2024-01-09 16:41:17 +0800

    fix: add volar support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,4 +2,86 @@ return {
 	{ import = "lazyvim.plugins.extras.lang.json" },
 	{ import = "lazyvim.plugins.extras.linting.eslint" },
 	{ import = "lazyvim.plugins.extras.formatting.prettier" },
+
+	{
+		"neovim/nvim-lspconfig",
+		---@class PluginLspOpts
+		opts = {
+			---@type lspconfig.options
+			servers = {
+				-- pyright will be automatically installed with mason and loaded with lspconfig
+				volar = {
+					filetypes = {
+						"typescript",
+						"javascript",
+						"javascriptreact",
+						"typescriptreact",
+						"vue",
+						"json",
+						"svelte",
+					},
+					settings = {
+						typescript = {
+							inlayHints = {
+								enumMemberValues = {
+									enabled = true,
+								},
+								functionLikeReturnTypes = {
+									enabled = true,
+								},
+								includeInlayFunctionParameterTypeHints = {
+									enabled = true,
+								},
+								parameterNames = {
+									enabled = true,
+									suppressWhenArgumentMatchesName = true,
+								},
+								parameterTypes = {
+									enabled = true,
+								},
+								propertyDeclarationTypes = {
+									enabled = true,
+								},
+								includeInlayFunctionLikeReturnTypeHints = {
+									enabled = true,
+								},
+								variableTypes = {
+									enabled = true,
+								},
+							},
+						},
+						javascript = {
+							inlayHints = {
+								enumMemberValues = {
+									enabled = true,
+								},
+								functionLikeReturnTypes = {
+									enabled = true,
+								},
+								includeInlayFunctionParameterTypeHints = {
+									enabled = true,
+								},
+								parameterNames = {
+									enabled = true,
+									suppressWhenArgumentMatchesName = true,
+								},
+								parameterTypes = {
+									enabled = true,
+								},
+								propertyDeclarationTypes = {
+									enabled = true,
+								},
+								includeInlayFunctionLikeReturnTypeHints = {
+									enabled = true,
+								},
+								variableTypes = {
+									enabled = true,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
 }
