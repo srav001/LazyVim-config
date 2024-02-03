895900632d6e024853c27ee5a5afad649504c613
Sravan <sravansuresh1x@gmail.com>
2024-02-03 19:37:00 +0800

    fix: minors

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,104 +3,98 @@ return {
 	{ import = "lazyvim.plugins.extras.linting.eslint" },
 	{ import = "lazyvim.plugins.extras.formatting.prettier" },
 	{ import = "lazyvim.plugins.extras.lang.tailwind" },
-	{ import = "lazyvim.plugins.extras.lang.typescript" },
 
 	{
 		"neovim/nvim-lspconfig",
 		---@class PluginLspOpts
 		opts = {
-			---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+			diagnostics = {
+				underline = true,
+				update_in_insert = false,
+				virtual_text = {
+					spacing = 4,
+					source = "if_many",
+					prefix = "●",
+				},
+				severity_sort = true,
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = require("lazyvim.config").icons.diagnostics.Error,
+						[vim.diagnostic.severity.WARN] = require("lazyvim.config").icons.diagnostics.Warn,
+						-- [vim.diagnostic.severity.HINT] = require("lazyvim.config").icons.diagnostics.Hint,
+						-- [vim.diagnostic.severity.INFO] = require("lazyvim.config").icons.diagnostics.Info,
+					},
+				},
+			},
+			capabilities = {
+				workspace = {
+					didChangeWatchedFiles = {
+						dynamicRegistration = true,
+					},
+				},
+			},
 			setup = {
-				-- ---@class lspconfig.options.volar
-				-- volar = function(_, opts)
-				-- 	opts.filetypes =
-				-- 		{ "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json", "svelte" }
-				-- 	opts.settings = {
-				-- 		typescript = {
-				-- 			inlayHints = {
-				-- 				enumMemberValues = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				functionLikeReturnTypes = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				includeInlayFunctionParameterTypeHints = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				parameterNames = {
-				-- 					enabled = true,
-				-- 					suppressWhenArgumentMatchesName = true,
-				-- 				},
-				-- 				parameterTypes = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				propertyDeclarationTypes = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				includeInlayFunctionLikeReturnTypeHints = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				variableTypes = {
-				-- 					enabled = true,
-				-- 				},
-				-- 			},
-				-- 		},
-				-- 		javascript = {
-				-- 			inlayHints = {
-				-- 				enumMemberValues = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				functionLikeReturnTypes = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				includeInlayFunctionParameterTypeHints = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				parameterNames = {
-				-- 					enabled = true,
-				-- 					suppressWhenArgumentMatchesName = true,
-				-- 				},
-				-- 				parameterTypes = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				propertyDeclarationTypes = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				includeInlayFunctionLikeReturnTypeHints = {
-				-- 					enabled = true,
-				-- 				},
-				-- 				variableTypes = {
-				-- 					enabled = true,
-				-- 				},
-				-- 			},
-				-- 		},
-				-- 	}
-				-- end,
-				---@class lspconfig.options.tsserver
-				tsserver = function(_, opts)
+				---@class lspconfig.options.volar
+				volar = function(_, opts)
+					opts.filetypes =
+						{ "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "svelte" }
 					opts.settings = {
 						typescript = {
 							inlayHints = {
-								includeInlayParameterNameHints = "all",
-								includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-								includeInlayFunctionParameterTypeHints = true,
-								includeInlayVariableTypeHints = true,
-								includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-								includeInlayPropertyDeclarationTypeHints = true,
-								includeInlayFunctionLikeReturnTypeHints = true,
-								includeInlayEnumMemberValueHints = true,
+								enumMemberValues = {
+									enabled = true,
+								},
+								functionLikeReturnTypes = {
+									enabled = true,
+								},
+								includeInlayFunctionParameterTypeHints = {
+									enabled = true,
+								},
+								parameterNames = {
+									enabled = true,
+									suppressWhenArgumentMatchesName = true,
+								},
+								parameterTypes = {
+									enabled = true,
+								},
+								propertyDeclarationTypes = {
+									enabled = true,
+								},
+								includeInlayFunctionLikeReturnTypeHints = {
+									enabled = true,
+								},
+								variableTypes = {
+									enabled = true,
+								},
 							},
 						},
 						javascript = {
 							inlayHints = {
-								includeInlayParameterNameHints = "all",
-								includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-								includeInlayFunctionParameterTypeHints = true,
-								includeInlayVariableTypeHints = true,
-								includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-								includeInlayPropertyDeclarationTypeHints = true,
-								includeInlayFunctionLikeReturnTypeHints = true,
-								includeInlayEnumMemberValueHints = true,
+								enumMemberValues = {
+									enabled = true,
+								},
+								functionLikeReturnTypes = {
+									enabled = true,
+								},
+								includeInlayFunctionParameterTypeHints = {
+									enabled = true,
+								},
+								parameterNames = {
+									enabled = true,
+									suppressWhenArgumentMatchesName = true,
+								},
+								parameterTypes = {
+									enabled = true,
+								},
+								propertyDeclarationTypes = {
+									enabled = true,
+								},
+								includeInlayFunctionLikeReturnTypeHints = {
+									enabled = true,
+								},
+								variableTypes = {
+									enabled = true,
+								},
 							},
 						},
 					}
