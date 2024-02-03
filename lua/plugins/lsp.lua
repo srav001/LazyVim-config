0e1cfc02829a0f73bf1e9db0642a5874ebe95623
Sravan <sravansuresh1x@gmail.com>
2024-02-03 15:13:00 +0800

    fix: swtich to tsserver

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
 	{ import = "lazyvim.plugins.extras.linting.eslint" },
 	{ import = "lazyvim.plugins.extras.formatting.prettier" },
 	{ import = "lazyvim.plugins.extras.lang.tailwind" },
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
 
 	{
 		"neovim/nvim-lspconfig",
@@ -10,100 +11,100 @@ return {
 		opts = {
 			---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
 			setup = {
-				---@class lspconfig.options.volar
-				volar = function(_, opts)
-					opts.filetypes =
-						{ "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json", "svelte" }
-					opts.settings = {
-						typescript = {
-							inlayHints = {
-								enumMemberValues = {
-									enabled = true,
-								},
-								functionLikeReturnTypes = {
-									enabled = true,
-								},
-								includeInlayFunctionParameterTypeHints = {
-									enabled = true,
-								},
-								parameterNames = {
-									enabled = true,
-									suppressWhenArgumentMatchesName = true,
-								},
-								parameterTypes = {
-									enabled = true,
-								},
-								propertyDeclarationTypes = {
-									enabled = true,
-								},
-								includeInlayFunctionLikeReturnTypeHints = {
-									enabled = true,
-								},
-								variableTypes = {
-									enabled = true,
-								},
-							},
-						},
-						javascript = {
-							inlayHints = {
-								enumMemberValues = {
-									enabled = true,
-								},
-								functionLikeReturnTypes = {
-									enabled = true,
-								},
-								includeInlayFunctionParameterTypeHints = {
-									enabled = true,
-								},
-								parameterNames = {
-									enabled = true,
-									suppressWhenArgumentMatchesName = true,
-								},
-								parameterTypes = {
-									enabled = true,
-								},
-								propertyDeclarationTypes = {
-									enabled = true,
-								},
-								includeInlayFunctionLikeReturnTypeHints = {
-									enabled = true,
-								},
-								variableTypes = {
-									enabled = true,
-								},
-							},
-						},
-					}
-				end,
-				-- ---@class lspconfig.options.tsserver
-				-- tsserver = function(_, opts)
+				-- ---@class lspconfig.options.volar
+				-- volar = function(_, opts)
+				-- 	opts.filetypes =
+				-- 		{ "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json", "svelte" }
 				-- 	opts.settings = {
 				-- 		typescript = {
 				-- 			inlayHints = {
-				-- 				includeInlayParameterNameHints = "all",
-				-- 				includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-				-- 				includeInlayFunctionParameterTypeHints = true,
-				-- 				includeInlayVariableTypeHints = true,
-				-- 				includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-				-- 				includeInlayPropertyDeclarationTypeHints = true,
-				-- 				includeInlayFunctionLikeReturnTypeHints = true,
-				-- 				includeInlayEnumMemberValueHints = true,
+				-- 				enumMemberValues = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				functionLikeReturnTypes = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				includeInlayFunctionParameterTypeHints = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				parameterNames = {
+				-- 					enabled = true,
+				-- 					suppressWhenArgumentMatchesName = true,
+				-- 				},
+				-- 				parameterTypes = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				propertyDeclarationTypes = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				includeInlayFunctionLikeReturnTypeHints = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				variableTypes = {
+				-- 					enabled = true,
+				-- 				},
 				-- 			},
 				-- 		},
 				-- 		javascript = {
 				-- 			inlayHints = {
-				-- 				includeInlayParameterNameHints = "all",
-				-- 				includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-				-- 				includeInlayFunctionParameterTypeHints = true,
-				-- 				includeInlayVariableTypeHints = true,
-				-- 				includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-				-- 				includeInlayPropertyDeclarationTypeHints = true,
-				-- 				includeInlayFunctionLikeReturnTypeHints = true,
-				-- 				includeInlayEnumMemberValueHints = true,
+				-- 				enumMemberValues = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				functionLikeReturnTypes = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				includeInlayFunctionParameterTypeHints = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				parameterNames = {
+				-- 					enabled = true,
+				-- 					suppressWhenArgumentMatchesName = true,
+				-- 				},
+				-- 				parameterTypes = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				propertyDeclarationTypes = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				includeInlayFunctionLikeReturnTypeHints = {
+				-- 					enabled = true,
+				-- 				},
+				-- 				variableTypes = {
+				-- 					enabled = true,
+				-- 				},
 				-- 			},
 				-- 		},
 				-- 	}
 				-- end,
+				---@class lspconfig.options.tsserver
+				tsserver = function(_, opts)
+					opts.settings = {
+						typescript = {
+							inlayHints = {
+								includeInlayParameterNameHints = "all",
+								includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+								includeInlayFunctionParameterTypeHints = true,
+								includeInlayVariableTypeHints = true,
+								includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+								includeInlayPropertyDeclarationTypeHints = true,
+								includeInlayFunctionLikeReturnTypeHints = true,
+								includeInlayEnumMemberValueHints = true,
+							},
+						},
+						javascript = {
+							inlayHints = {
+								includeInlayParameterNameHints = "all",
+								includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+								includeInlayFunctionParameterTypeHints = true,
+								includeInlayVariableTypeHints = true,
+								includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+								includeInlayPropertyDeclarationTypeHints = true,
+								includeInlayFunctionLikeReturnTypeHints = true,
+								includeInlayEnumMemberValueHints = true,
+							},
+						},
+					}
+				end,
 				-- Specify * to use this function as a fallback for any server
 				["*"] = function(server, opts) end,
 			},
