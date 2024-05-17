67e768b6fff962973f21ae69e23413d84b17aa51
Sravan <sravansuresh1x@gmail.com>
2024-05-18 01:49:37 +0530

    fix: create repro

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,3 @@
 return {
-	{ import = "lazyvim.plugins.extras.lang.json" },
-	{ import = "lazyvim.plugins.extras.linting.eslint" },
-	{ import = "lazyvim.plugins.extras.formatting.prettier" },
-	{ import = "lazyvim.plugins.extras.lang.tailwind" },
-	{ import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
 }
