f4a545155c328409ee76ddeb8dce9a25d5590507
Sravan <sravansuresh1x@gmail.com>
2024-05-21 02:02:15 +0530

    fix: commit nvim-cmp to version

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,16 +1,10 @@
 return {
-  { import = "lazyvim.plugins.extras.coding.luasnip" },
   -- Use <tab> for completion and snippets (supertab)
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
-  -- then: setup supertab in cmp
-  {
     "hrsh7th/nvim-cmp",
+    commit = "b356f2c",
+    pin = true,
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local has_words_before = function()
