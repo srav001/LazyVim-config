cc580c2234ec02f587c5631e65c7a62017a4ef50
Sravan <sravansuresh1x@gmail.com>
2024-05-21 02:37:04 +0530

    fix: remove mini ai

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,30 +1,34 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-mocha",
-    },
-  },
-  {
-    "zbirenbaum/copilot.lua",
-    event = { "InsertEnter" },
-    cmd = { "Copilot" },
-    opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        debounce = 75,
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          dismiss = "<C-o>",
-        },
-      },
-    },
-  },
-  {
-    "rcarriga/nvim-notify",
-    enabled = false,
-  },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-mocha",
+		},
+	},
+	{
+		"zbirenbaum/copilot.lua",
+		event = { "InsertEnter" },
+		cmd = { "Copilot" },
+		opts = {
+			suggestion = {
+				enabled = true,
+				auto_trigger = true,
+				debounce = 75,
+				keymap = {
+					accept = "<C-l>",
+					accept_word = false,
+					accept_line = false,
+					dismiss = "<C-o>",
+				},
+			},
+		},
+	},
+	{
+		"rcarriga/nvim-notify",
+		enabled = false,
+	},
+	{
+		"echasnovski/mini.ai",
+		enabled = false,
+	},
 }
