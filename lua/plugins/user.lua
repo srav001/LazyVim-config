d2f10b30c4190c08f0ba88a4f3235b267cd00c1c
Sravan <sravansuresh1x@gmail.com>
2024-07-29 02:21:09 +0530

    fix: add maven

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -5,23 +5,35 @@ return {
 			colorscheme = "catppuccin-mocha",
 		},
 	},
+	-- {
+	-- 	"zbirenbaum/copilot.lua",
+	-- 	event = { "InsertEnter" },
+	-- 	cmd = { "Copilot" },
+	-- 	opts = {
+	-- 		suggestion = {
+	-- 			enabled = true,
+	-- 			auto_trigger = true,
+	-- 			debounce = 75,
+	-- 			keymap = {
+	-- 				accept = "<C-l>",
+	-- 				accept_word = false,
+	-- 				accept_line = false,
+	-- 				dismiss = "<C-o>",
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 	{
-		"zbirenbaum/copilot.lua",
-		event = { "InsertEnter" },
-		cmd = { "Copilot" },
-		opts = {
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				debounce = 75,
-				keymap = {
-					accept = "<C-l>",
-					accept_word = false,
-					accept_line = false,
-					dismiss = "<C-o>",
+		"supermaven-inc/supermaven-nvim",
+		config = function()
+			require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<C-l>",
+					clear_suggestion = "<C-]>",
+					accept_word = "<C-j>",
 				},
-			},
-		},
+			})
+		end,
 	},
 	{
 		"rcarriga/nvim-notify",
