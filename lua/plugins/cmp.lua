f89cde51e6a6d090708d844ec391cb4ab40f4832
Sravan <sravansuresh1x@gmail.com>
2024-08-03 23:07:50 +0530

    fix: remove lock files

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,8 +3,6 @@ return {
 	-- first: disable default <tab> and <s-tab> behavior in LuaSnip
 	{
 		"hrsh7th/nvim-cmp",
-		-- commit = "b356f2c",
-		-- pin = true,
 		-- ---@param opts cmp.ConfigSchema
 		opts = function(_, opts)
 			local has_words_before = function()
