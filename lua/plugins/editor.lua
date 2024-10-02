59a88727946af8f6262e6442ceeb35eda871d240
Sravan <sravansuresh1x@gmail.com>
2024-10-02 20:34:24 +0530

    feat: upadte lock and disable edit

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,16 +7,16 @@ return {
 			},
 		},
 	},
-	{
-		"telescope.nvim",
-		dependencies = {
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-			config = function()
-				require("telescope").load_extension("fzf")
-			end,
-		},
-	},
+	-- {
+	-- 	"telescope.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-telescope/telescope-fzf-native.nvim",
+	-- 		build = "make",
+	-- 		config = function()
+	-- 			require("telescope").load_extension("fzf")
+	-- 		end,
+	-- 	},
+	-- },
 	{
 		"echasnovski/mini.move",
 		version = "*",
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -6,36 +6,18 @@ return {
 		},
 	},
 	-- {
-	-- 	"zbirenbaum/copilot.lua",
-	-- 	event = { "InsertEnter" },
-	-- 	cmd = { "Copilot" },
-	-- 	opts = {
-	-- 		suggestion = {
-	-- 			enabled = true,
-	-- 			auto_trigger = true,
-	-- 			debounce = 75,
-	-- 			keymap = {
-	-- 				accept = "<C-l>",
-	-- 				accept_word = false,
-	-- 				accept_line = false,
-	-- 				dismiss = "<C-o>",
+	-- 	"supermaven-inc/supermaven-nvim",
+	-- 	event = "InsertEnter",
+	-- 	config = function()
+	-- 		require("supermaven-nvim").setup({
+	-- 			keymaps = {
+	-- 				accept_suggestion = "<C-l>",
+	-- 				clear_suggestion = "<C-]>",
+	-- 				accept_word = "<C-j>",
 	-- 			},
-	-- 		},
-	-- 	},
+	-- 		})
+	-- 	end,
 	-- },
-	{
-		"supermaven-inc/supermaven-nvim",
-		event = "InsertEnter",
-		config = function()
-			require("supermaven-nvim").setup({
-				keymaps = {
-					accept_suggestion = "<C-l>",
-					clear_suggestion = "<C-]>",
-					accept_word = "<C-j>",
-				},
-			})
-		end,
-	},
 	{
 		"rcarriga/nvim-notify",
 		enabled = false,
