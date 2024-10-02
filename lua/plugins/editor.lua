37ddb1e50d2be013c7a2d0e9a9d21c43957ddb40
Sravan <sravansuresh1x@gmail.com>
2024-10-02 21:39:03 +0530

    fix: set version on svelte plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,16 +7,6 @@ return {
 			},
 		},
 	},
-	-- {
-	-- 	"telescope.nvim",
-	-- 	dependencies = {
-	-- 		"nvim-telescope/telescope-fzf-native.nvim",
-	-- 		build = "make",
-	-- 		config = function()
-	-- 			require("telescope").load_extension("fzf")
-	-- 		end,
-	-- 	},
-	-- },
 	{
 		"echasnovski/mini.move",
 		version = "*",
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,4 +5,13 @@ return {
 	{ import = "lazyvim.plugins.extras.lang.tailwind" },
 	{ import = "lazyvim.plugins.extras.lang.svelte" },
 	{ import = "lazyvim.plugins.extras.lang.typescript" },
+
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"svelte@0.17.0",
+			},
+		},
+	},
 }
