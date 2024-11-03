4bdc1e38dabf511b05949f0ab63270d5a0ab26b1
Sravan <sravansuresh1x@gmail.com>
2024-11-03 10:27:53 +0530

    feat: avante

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -6,6 +6,15 @@ return {
 		},
 	},
 	{
+		"rcarriga/nvim-notify",
+		enabled = false,
+	},
+	{
+		"echasnovski/mini.ai",
+		enabled = false,
+	},
+	-- AI items
+	{
 		"supermaven-inc/supermaven-nvim",
 		event = "InsertEnter",
 		config = function()
@@ -19,11 +28,50 @@ return {
 		end,
 	},
 	{
-		"rcarriga/nvim-notify",
-		enabled = false,
-	},
-	{
-		"echasnovski/mini.ai",
-		enabled = false,
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- set this if you want to always pull the latest change
+		opts = {
+			provider = "openai",
+			auto_suggestions_provider = "claude",
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
 	},
 }
