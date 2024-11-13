01d5c89dea23130aa553c3e615312149fe49c7bc
Sravan <sravansuresh1x@gmail.com>
2024-11-13 23:30:56 +0530

    fix: minor

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -36,19 +36,15 @@ return {
 			provider = "openai",
 			auto_suggestions_provider = "claude",
 		},
-		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 		build = "make",
-		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 			"stevearc/dressing.nvim",
 			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
-			--- The below dependencies are optional,
 			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 			"zbirenbaum/copilot.lua", -- for providers='copilot'
 			{
-				-- support for image pasting
 				"HakonHarnes/img-clip.nvim",
 				event = "VeryLazy",
 				opts = {
