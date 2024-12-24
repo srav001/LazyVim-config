561cc827dd3f7d1e6718aa660181c07d06b38455
Sravan <sravansuresh1x@gmail.com>
2024-12-24 14:26:02 +0800

    feat: use lazyextras

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,12 +1,4 @@
 return {
-	-- { import = "lazyvim.plugins.extras.lang.json" },
-	-- { import = "lazyvim.plugins.extras.linting.eslint" },
-	-- { import = "lazyvim.plugins.extras.formatting.prettier" },
-	-- { import = "lazyvim.plugins.extras.lang.tailwind" },
-	-- { import = "lazyvim.plugins.extras.lang.svelte" },
-	-- { import = "lazyvim.plugins.extras.lang.vue" },
-	-- { import = "lazyvim.plugins.extras.lang.typescript" },
-
 	{
 		"williamboman/mason-lspconfig.nvim",
 		opts = {
