642239a5bf350cee26f31fbf5400c44b876835f9
Sravan <sravansuresh1x@gmail.com>
2025-02-25 19:09:26 +0800

    fix: remove ensure installed

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,13 +1,5 @@
 return {
 	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {
-				"svelte@0.17.0",
-			},
-		},
-	},
-	{
 		"simrat39/symbols-outline.nvim",
 		cmd = "SymbolsOutline",
 		keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
