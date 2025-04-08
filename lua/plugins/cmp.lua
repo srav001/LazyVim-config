e3d118f29e7a8ad80bbe5aa944c839a25dd0db3d
Sravan <sravansuresh1x@gmail.com>
2025-04-09 01:07:15 +0530

    feat: update

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -30,7 +30,6 @@ return {
 			local cmp = require("cmp")
 
 			opts.mapping = vim.tbl_extend("force", opts.mapping, {
-
 				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						-- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
