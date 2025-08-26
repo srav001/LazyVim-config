1cc5951f9da14dbabe10bd64d8162702f9123cbf
Sravan Suresh <sravansuresh@Sravans-MacBook-Air.local>
2025-08-26 14:40:51 +0530

    fix: catpuccin patch

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -1,4 +1,15 @@
 return {
+	-- Catppuccin breaking changes patch
+	{
+		"catppuccin/nvim",
+		opts = function(_, opts)
+			local module = require("catppuccin.groups.integrations.bufferline")
+			if module then
+				module.get = module.get_theme
+			end
+			return opts
+		end,
+	},
 	{
 		"LazyVim/LazyVim",
 		lazy = false,
@@ -27,16 +38,6 @@ return {
 					accept_word = "<C-j>",
 				},
 			})
-		end,
-	},
-	-- https://github.com/LazyVim/LazyVim/pull/6354#issuecomment-3202799735
-	{
-		"akinsho/bufferline.nvim",
-		init = function()
-			local bufline = require("catppuccin.groups.integrations.bufferline")
-			function bufline.get()
-				return bufline.get_theme()
-			end
 		end,
 	},
 }
