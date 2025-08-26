401f65be2e26e2f6cadeed0b48af1ef85e8f27f8
Sravan Suresh <sravansuresh@Sravans-MacBook-Air.local>
2025-08-26 14:26:45 +0530

    fix: remove tiny indent

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -38,18 +38,17 @@ require("lazy").setup({
 	},
 })
 
-require("tiny-inline-diagnostic").setup({
-	signs = {
-		left = "",
-		right = "",
-		diag = "●",
-		arrow = "    ",
-		up_arrow = "    ",
-		vertical = " │",
-		vertical_end = " └",
-	},
-	blend = {
-		factor = 0.22,
-	},
-})
-
+-- require("tiny-inline-diagnostic").setup({
+-- 	signs = {
+-- 		left = "",
+-- 		right = "",
+-- 		diag = "●",
+-- 		arrow = "    ",
+-- 		up_arrow = "    ",
+-- 		vertical = " │",
+-- 		vertical_end = " └",
+-- 	},
+-- 	blend = {
+-- 		factor = 0.22,
+-- 	},
+-- })
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -30,13 +30,13 @@ return {
 		"mg979/vim-visual-multi",
 		event = "VeryLazy",
 	},
-	{
-		"rachartier/tiny-inline-diagnostic.nvim",
-		event = "VeryLazy", -- Or `LspAttach`
-		priority = 1000,
-		config = function()
-			require("tiny-inline-diagnostic").setup()
-			vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
-		end,
-	},
+	-- {
+	-- 	"rachartier/tiny-inline-diagnostic.nvim",
+	-- 	event = "VeryLazy", -- Or `LspAttach`
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("tiny-inline-diagnostic").setup()
+	-- 		vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+	-- 	end,
+	-- },
 }
